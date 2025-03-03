b822d64cd408aa77209a92700a0f5df6e0d2e46a
st10443220 <st10443220@vcconnect.edu.za>
2025-03-03 21:11:35 +0200

    Added WelcomeAudio.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Cyber_Awareness_Chatbot
+﻿using Cyber_Awareness_Chatbot.Audio;
+
+namespace Cyber_Awareness_Chatbot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Audio.Audio audio = new Audio.Audio();
+            audio.LoadWelcomeMessageAudio();
+            Console.ReadKey();
         }
     }
 }
