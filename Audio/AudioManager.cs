224c07df58692ea0f9ece2d4aa273bb5a06b19cc
st10443220 <st10443220@vcconnect.edu.za>
2025-03-03 21:53:50 +0200

    Added AudioStreamMethod and ASCII Art

---
+++ Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using Cyber_Awareness_Chatbot.Properties;
+
+/*
+ * https://stackoverflow.com/questions/1900707/how-to-play-audio-from-resource
+ */
+
+namespace Cyber_Awareness_Chatbot.Audio
+{
+    class AudioManager
+    {
+        public void PlayWelcomeMessage()
+        {
+            // Plays the audio file that is stored in the programs resources.
+            PlayByteArray(Resources.about_time);
+        }
+
+        public void PlayByteArray(Byte[] audio)
+        {
+            // Converts the given byte[] to a stream.
+            using (var audioStream = new MemoryStream(audio))
+            {
+                // Creates the sound player to play an audio stream.
+                using (var player = new SoundPlayer(audioStream))
+                {
+                    // Plays the audio stream.
+                    player.Play();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using Cyber_Awareness_Chatbot.Audio;
+using Cyber_Awareness_Chatbot.Properties;
 
 namespace Cyber_Awareness_Chatbot
 {
     internal class Program
     {
+        static AudioManager audio = new AudioManager();
         static void Main(string[] args)
         {
-            Audio.Audio audio = new Audio.Audio();
-            audio.LoadWelcomeMessageAudio();
+            // Play the welcome message!
+            audio.PlayWelcomeMessage();
+
+            // Display the ASCII art upon launch.
+            Console.WriteLine(Resources.hacker_logo);
+
             Console.ReadKey();
         }
     }
